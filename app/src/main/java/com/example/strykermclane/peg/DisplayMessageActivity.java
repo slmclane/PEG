e1c45ca98a443b85dc439a408ddbfb7532157488
Stryker McLane <strykermclane@Dbook.local>
2016-01-30 15:41:44 -0900

    Initial commit

---
+++ app/src/main/java/com/example/strykermclane/peg/DisplayMessageActivity.java
@@ -0,0 +1,42 @@
+package com.example.strykermclane.peg;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+public class DisplayMessageActivity extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_display_message);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+
+            }
+        });
+        Intent intent = getIntent();
+        String message = intent.getStringExtra(LandingPage.EXTRA_MESSAGE);
+        TextView textView = new TextView(this);
+        textView.setTextSize(20);
+        textView.setText(message);
+        LinearLayout layout = (LinearLayout) findViewById(R.id.content);
+        layout.addView(textView);
+    }
+
+}
