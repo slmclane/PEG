8f4a4edb84916653f01b9e431ae63c3a0364a2a2
slmclane <slmclane@alaska.edu>
2016-04-01 17:41:03 -0800

    Added recovery tracker output file methods?

--- app/src/main/java/com/example/strykermclane/peg/MyCompassView.java
+++ app/src/main/java/com/example/strykermclane/peg/MyCompassView.java
@@ -9,8 +9,18 @@ import android.graphics.Paint;
 import android.graphics.Path;
 import android.graphics.RectF;
 import android.graphics.SweepGradient;
+import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 
 public class MyCompassView extends View {
 
@@ -184,8 +194,6 @@ public class MyCompassView extends View {
 
 
 
-
-
         if(exerSwitchCompass == 0) {
             position -= defaultDeviation;
         }
@@ -234,6 +242,50 @@ public class MyCompassView extends View {
             vibeSwitch = true;
         }
         invalidate();
+    }
+
+    void syncUserMaximum(){
+        Context context = this.getContext();
+        String maxUpperString = Integer.toString(maxUpper);
+        String maxLowerString = Integer.toString(maxLower);
+        String userMax = maxUpperString +","+ maxLowerString;
+        try {
+            OutputStreamWriter outputStreamWriter = new OutputStreamWriter(context.openFileOutput("recovery.txt", Context.MODE_PRIVATE));
+            outputStreamWriter.write(maxUpperString);
+            outputStreamWriter.close();
+        }
+        catch (IOException e) {
+            Log.e("Exception", "File write failed: " + e.toString());
+        }
+    }private String readFromFile() {
+        Context context = this.getContext();
+
+        String ret = "";
+
+        try {
+            InputStream inputStream = context.openFileInput("recovery.txt");
+
+            if ( inputStream != null ) {
+                InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
+                BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+                String receiveString = "";
+                StringBuilder stringBuilder = new StringBuilder();
+
+                while ( (receiveString = bufferedReader.readLine()) != null ) {
+                    stringBuilder.append(receiveString);
+                }
+
+                inputStream.close();
+                ret = stringBuilder.toString();
+            }
+        }
+        catch (FileNotFoundException e) {
+            Log.e("login activity", "File not found: " + e.toString());
+        } catch (IOException e) {
+            Log.e("login activity", "Can not read file: " + e.toString());
+        }
+
+        return ret;
     }
 
 }
