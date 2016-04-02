3f121085ce81752b4c7b3361a2b50bc197077783
strykermclane <slmclane@alaska.edu>
2016-04-01 16:08:22 -0800

    added Trent's spinner

--- app/src/main/java/com/example/strykermclane/peg/HandExercises.java
+++ app/src/main/java/com/example/strykermclane/peg/HandExercises.java
@@ -2,13 +2,13 @@ package com.example.strykermclane.peg;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class HandExercises extends AppCompatActivity {
 
@@ -19,6 +19,16 @@ public class HandExercises extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         setTitle("Hand Exercises");
+
+        Spinner spinner = (Spinner) findViewById(R.id.spinner);
+// Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.choices, R.layout.spinner);
+// Specify the layout to use when the list of choices appears
+        adapter.setDropDownViewResource(R.layout.spinner_drop_down);
+// Apply the adapter to the spinner
+        spinner.setAdapter(adapter);
+
 
     }
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -82,5 +92,48 @@ public class HandExercises extends AppCompatActivity {
         intentFlexionExtension.putExtra("exerSwitch",2);
         startActivity(intentFlexionExtension);
 
+    }
+
+    public void goToExercise(View view){
+        Spinner spinner = (Spinner) findViewById(R.id.spinner);
+        if (spinner.getSelectedItem().toString().equals("Flexion Extension")){
+            /**
+             * This method bring the user to the next screen.
+             *
+             *show the usage of various javadoc Tags.
+             * @param view
+             * @return Nothing
+             */
+            int exerSwitch = 3;
+            Intent intentFlexionExtension = new Intent(this, exerciseActivity.class);
+            intentFlexionExtension.putExtra("exerSwitch",2);
+            startActivity(intentFlexionExtension);
+        }
+        else if(spinner.getSelectedItem().toString().equals("Pronation Supination")){
+            /**
+             * This method bring the user to the next screen.
+             *
+             *show the usage of various javadoc Tags.
+             * @param view
+             * @return Nothing
+             */
+            int exerSwitch = 3;
+            Intent intentPronationSupination = new Intent(this, exerciseActivity.class);
+            intentPronationSupination.putExtra("exerSwitch",1);
+            startActivity(intentPronationSupination);
+        }
+        else if(spinner.getSelectedItem().toString().equals("Radial Ulnar Deviation")){
+            /**
+             * This method bring the user to the next screen.
+             *
+             *show the usage of various javadoc Tags.
+             * @param view
+             * @return Nothing
+             */
+            int exerSwitch = 3;
+            Intent intentRadialUlnarDeviation = new Intent(this, exerciseActivity.class);
+            intentRadialUlnarDeviation.putExtra("exerSwitch",0);
+            startActivity(intentRadialUlnarDeviation);
+        }
     }
 }
